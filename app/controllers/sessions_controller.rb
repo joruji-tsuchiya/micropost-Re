4e3073969083c74a0b6df406f8849c44eb83e9e9
joruji.tsuchiya <joruji39_1511530041y@icloud.com>
2018-11-24 17:37:22 +0000

    post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    email = param[:session][:email].downcase
+    email = params[:session][:email].downcase
     password = params[:session][:password]
     if login(email, password)
       flash[:success] = 'ログインに成功しました。'
-      redirect_to @users
+      redirect_to @user
     else
       flash.now[:danger] = 'ログインに失敗しました。'
       render 'new'
